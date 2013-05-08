bf1798b1332fd315a8608ee0efd784a5cfa487fd
Farhan Mannan <farhanmannan@mac.com>
2013-05-08 19:06:58 +0100

    Configure RSpec generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Destroyer
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.test_framework :rspec, routing_specs: false, views: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
