afac5908cd98fd42ef4fd2f377f075cc19839e24
Farhan Mannan <farhanmannan@mac.com>
2013-05-08 19:06:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Destroyer::Application.config.secret_token = 'd2edab7ae89a1583265e771051bf48c9258e0045cfbb7a04853d8bf3e8422e7c61ae0e01859ef1c613a8a9eef7baefebb9361d96c0e1ceb818dfce4b7fbfd849'
