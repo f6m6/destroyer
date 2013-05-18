76beba1bbb2b7c35597eccd7c7bc0c799d02ecbf
Farhan Mannan <farhanmannan@mac.com>
2013-05-18 17:00:13 +0100

    UrlValidator now guards against some classes of invalid URLs

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,6 +1,14 @@
+require 'uri'
+
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.include?(".")
+    def valid?(url)
+      uri = URI.parse(url)
+      uri.kind_of?(URI::HTTP)
+    rescue URI::InvalidURIError
+      false
+    end
+    unless valid?('http://' + value)
       record.errors[attribute] << (options[:message] || "is not a valid URL")
     end
   end
