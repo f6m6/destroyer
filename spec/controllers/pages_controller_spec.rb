6c6ce9b04e9daec3c53a7ce47a29b0645aa9e750
Farhan Mannan <farhanmannan@mac.com>
2013-05-24 14:08:01 +0100

    More tests for PageController

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -22,16 +22,29 @@ describe PagesController do
       post :create, page: {url: '::::', content: nil}
       response.should render_template :new
     end
+    it "responds with an existing page if it exists" do
+      p = Page.create(url: 'test.com', content: 'test')
+      post :create, page: { url: 'test.com', content: 'test' }
+      assigns(:existing_page).url.should == 'test.com'
+    end
   end
 
   describe "GET #show" do
-    it "assigns the requested page to @page" do
-      page = Page.create(url: 'test.com', content: 'html')
-      get :show, id: page.id
+    it "retrieves and assigns the requested page to @page" do
+      p = Page.create(url: 'test.com', content: 'html')
+      get :show, id: p.id
       assigns(:page).url.should == 'test.com'
     end
     it "renders the :show template" do
       response.status.should eq 200
+    end
+    it "creates and assigns the page if it doesn't already exist" do
+      get :show, id: 'yahoo.com'
+      assigns(:page).url.should == 'yahoo.com'
+    end
+    it "renders new if it can't save the page" do
+      get :show, id: ':::::'
+      assert_template :new
     end
   end
 end
